0ea351af903dc0b78c5b893f8772c149ad7879fd
Ernesto Gomez Romero <ernestogomez.ing@gmail.com>
2026-09-24 19:28:57 -0600

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config/conexion.php'; 
-require_once 'config/Security.php';
-require_once 'controllers/LoginController.php';
-
-$controller = new LoginController($conexion);
-$controller->handleRequest();
-?>
